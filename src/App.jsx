1e6104924bfe7c7ddc164938065c53f7dfcb22d7
wallawaz <bwallad@gmail.com>
2018-01-07 19:37:04 -0500

    move App component to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+import AddUser from './components/AddUser';
+import UsersList from './components/UsersList';
+
+class App extends Component {
+    constructor() {
+      super()
+      this.state = {
+          users: [],
+          username: '',
+          email: ''
+      }
+    }
+    componentDidMount() {
+        this.getUsers()
+    }
+    handleChange(event) {
+        const obj = {};
+        obj[event.target.name] = event.target.value;
+        this.setState(obj);
+    }
+    getUsers() {
+        axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+            .then((res) => { this.setState({ users: res.data.data.users }); })
+            .catch((err) => { console.log(err); })
+    }
+    addUser(event) {
+        event.preventDefault();
+        const data = {
+            username: this.state.username,
+            email: this.state.email
+        };
+        axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+            .then((res) =>  {
+                this.getUsers();
+                this.setState({username: '', email: ''})
+            })
+            .catch((err) => { console.log(err); })
+    }
+    render() {
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <br/>
+              <h1>All Users</h1>
+              <hr/><br/>
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                handleChange={this.handleChange.bind(this)}
+                addUser={this.addUser.bind(this)}
+              />
+             <UsersList users={this.state.users}/>
+            </div>
+          </div>
+        </div>
+      )
+    }
+  }
+  
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
